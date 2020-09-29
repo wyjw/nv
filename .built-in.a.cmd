f94f663c4b24f2e035926509af948f830ceb13d9
wyjw <yujian.wu1@gmail.com>
2020-09-29 19:55:27 +0800

    more stuff deleted

--- .built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/nvme/host/built-in.a := rm -f drivers/nvme/host/built-in.a; ar cDPrST drivers/nvme/host/built-in.a drivers/nvme/host/core.o drivers/nvme/host/trace.o drivers/nvme/host/multipath.o drivers/nvme/host/lightnvm.o drivers/nvme/host/hwmon.o drivers/nvme/host/depthpath.o drivers/nvme/host/pci.o
